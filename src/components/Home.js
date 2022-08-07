a7c4b8225d3c14a9a2c57409b5279949c4a0025c
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-07 10:41:31 +0530

    💄 ⚡️ Styled the Sider and Header sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
+import moolLogo from '../assets/mool-logo.svg';
 import 'antd/dist/antd.min.css';
-import { Layout } from 'antd';
+import { HomeTwoTone, BellOutlined, QuestionCircleOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout, Button } from 'antd';
 import React, { useState } from 'react';
 const { Header, Content, Sider } = Layout;
 
@@ -12,22 +14,51 @@ const Home = () => {
       }}
     >
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="logo" />
-        <h1>This is the sider section!</h1>
+        <div className="logo">
+          <img src={moolLogo} alt="logo" style={{
+            width: '40%',
+            marginLeft: '8%',
+            marginTop: '12%',
+          }} />
+        </div>
+        <Button type='primary' icon={<HomeTwoTone twoToneColor='#5d77e2' />} size='large' style={{
+          textAlign: 'left',
+          width: '90%',
+          marginLeft: '5%',
+          marginTop: '35%',
+          backgroundColor: '#405CD2',
+        }}> Home </Button>
       </Sider>
       <Layout className="site-layout">
         <Header
           className="site-layout-background"
           style={{
-            backgroundColor: "rgb(240 242 245)",
+            backgroundColor: "#f7f7fa",
             padding: 0,
+            borderBottom: '3px solid #e8e8e8',
           }}
         >
-          <h1>This is the header section!</h1>
+          <div className='header-flexbox'>
+            <div className='header-flexbox-left'>
+              <HomeTwoTone twoToneColor='#5d77e2' style={{
+                fontSize: '20px',
+                marginTop: '22px',
+              }} />
+              <h2 className='header-flexbox-text'>Home</h2>
+            </div>
+            <div className='header-flexbox-right'>
+              <BellOutlined style={{ fontSize: '20px', marginTop: '22px' }} />
+              <QuestionCircleOutlined style={{ fontSize: '20px', marginTop: '22px' }} />
+              <SettingOutlined style={{ fontSize: '20px', marginTop: '22px' }} />
+                <div className='user-circle'>
+                <UserOutlined style={{ fontSize: '20px', marginTop: '22px', color: 'white' }} />
+              </div>
+            </div>
+          </div>
         </Header>
         <Content
           style={{
-            margin: '0 16px',
+            backgroundColor: "#f7f7fa",
           }}
         >
           <h1>This is the content section!</h1>
